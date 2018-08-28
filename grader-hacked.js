92420a6fb575d136175eacd159f408c72e262437
Tan Yuanhong <995721782@qq.com>
2018-08-28 22:55:41 +0800

    grader v0.1

--- grader-hacked.js
+++ grader-hacked.js
@@ -10,26 +10,21 @@ var tester_lib;
 var reviewer_lib;
 
 
-const expressions = [["f(nova_bb, 9);", "f(nova_bb, 8);"]];
+const expressions = [[  "f(nova_bb, 9);", 
+                        "f(nova_bb, 8);",
+                        "f(make_cross(rcross_bb), 5)",
+                        "f(make_cross(rcross_bb), 9)"]];
 
 
 for (var i = 1; i <= number_of_missions; i++){
-    // console.log("eh");
     for (expression of expressions[i-1]){
-        // console.log(expressions[i-1]);
-        // console.log(reviewee_src_path + i);
         reviewer_lib = require(reviewee_src_path + i);
-        // reviewer_lib.f();
         var reviewer_pic = eval("reviewer_lib." + expression);
-        // reviewer_lib.eval(expression);
         var tester_pic;
         for(tester of testers){
             tester_lib = require("./" + tester + "/" + mission_name + "/" + i);
             tester_pic = eval("tester_lib." + expression);
         }
         console.log(__are_pictures_equal(reviewer_pic, tester_pic));
-        // show(reviewer_pic);
     }
 }
-
-// console.log(tester_src_path);
