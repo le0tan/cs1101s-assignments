007a21bd4cd55113130cbbbf5ecc93583b3d2672
Tan Yuanhong <leotan.yh@gmail.com>
2018-09-11 15:17:11 +0800

    cm-tyh

--- tyh/curve-manipulation/2.js
+++ tyh/curve-manipulation/2.js
@@ -2,9 +2,9 @@
 
 function connect_ends(curve1, curve2) {
     // your program here
-    p1 = curve1(1);
-    p2 = curve2(0);
-    x_diff = x_of(p1) - x_of(p2);
-    y_diff = y_of(p1) - y_of(p2);
+    const p1 = curve1(1);
+    const p2 = curve2(0);
+    const x_diff = x_of(p1) - x_of(p2);
+    const y_diff = y_of(p1) - y_of(p2);
     return connect_rigidly(curve1, translate(x_diff, y_diff)(curve2));
 }
