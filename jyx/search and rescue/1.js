d7eed1e5e8ca156a945e4b78628e10e3f3598f7a
Jiang Yuxin <jiangyuxin@JiangdeMacBook-Pro.local>
2018-09-12 19:18:14 +0800

    m

---
+++ jyx/search and rescue/1.js
@@ -0,0 +1,12 @@
+function binary_search_tree_to_string(bst) {
+    if(is_empty_binary_tree(bst)) {
+        return "";
+    } else {
+        return binary_search_tree_to_string(left_subtree_of(bst))
+               + value_of(bst) + "\n"
+               + binary_search_tree_to_string(right_subtree_of(bst));
+    }
+}
+
+// Test
+ binary_search_tree_to_string(cadet_names);
---
+++ jyx/search and rescue/3.js
@@ -0,0 +1,23 @@
+// Part 1
+/* list([], "a", 
+        list([], "b", 
+             list([], "c", 
+                 list([], "d", 
+                      list([], "e",
+                            list([], "f",
+                                 list([], "hello", [])))))));
+*/
+
+// Part 2
+/* list(list(list([], "1", []), 
+            "2", 
+            list([], "3", [])), 
+        "4", 
+        list(list([], "5", []),
+             "6",
+             list([], "7", [])));
+*/
+
+// Part 3
+// Theta(log(n))
+
