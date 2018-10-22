7ad976440f8c8bc53e7adf8270f74417b68c74bb
Tan Yuanhong <leotan.yh@gmail.com>
2018-10-22 19:39:42 +0800

    tyh

--- tyh/reflection-and-studio/sicp.ch3.js
+++ tyh/reflection-and-studio/sicp.ch3.js
@@ -93,3 +93,132 @@ function make_account(x, pswd){
     }
     return f;
 }
+
+//3.18
+
+function cyc(xs){
+    function ap(arr, x){
+        arr[array_length(arr)] = x;
+    }
+    
+    let vis = [];
+    
+    function helper(lst){
+        if(is_empty_list(lst)){
+            return false;
+        } else {
+            let i = 0;
+            let ok = true;
+            while(vis[i] !== undefined){
+                if(vis[i] === lst){
+                    ok = false;
+                    break;
+                } else {
+                    i = i + 1;
+                }
+            }
+            if(ok){
+                vis[i] = lst;
+                return helper(tail(lst));
+            } else {
+                return true;
+            }
+        }
+    }
+    return helper(xs);
+}
+
+const a = pair(1, []);
+const b = pair([], a);
+set_tail(a, b);
+
+//3.19
+
+function cyc2(xs){
+    
+    function helper(one, two){
+        if(is_empty_list(one) || is_empty_list(two) || is_empty_list(tail(two))){
+            return false;
+        } else {
+            if(one === two){
+                return true;
+            } else {
+                return helper(tail(one), tail(tail(two)));   
+            }
+        }
+    }
+    
+    if(is_empty_list(xs)){
+        return false;
+    } else if(is_empty_list(tail(xs))){
+        return false;
+    } else {
+        return helper(tail(xs), tail(tail(xs)));    
+    }
+}
+
+//3.21
+
+function make_queue(){
+    return pair([],[]);
+}
+
+function front_ptr(queue){
+    return head(queue);
+}
+
+function end_ptr(queue){
+    return tail(queue);
+}
+
+function is_empty_queue(queue){
+    return is_empty_list(front_ptr(queue));
+}
+
+function set_front(queue, x){
+    set_head(queue, x);
+}
+
+function set_end(queue, x){
+    set_tail(queue, x);
+}
+
+function dequeue(queue){
+    if(is_empty_queue(queue)){
+        return 'aaa';
+    } else {
+        set_front(queue, tail(front_ptr(queue)));
+    }
+}
+
+function insert(queue, x){
+    const n = pair(x, []);
+    if(is_empty_queue(queue)){
+        set_front(queue, n);
+        set_end(queue, n);
+    } else {
+        set_tail(end_ptr(queue), n);
+        set_end(queue, n);
+    }
+}
+
+function print_queue(queue){
+    display(front_ptr(queue));
+}
+
+const a = make_queue();
+insert(a, 1);
+insert(a, 1);
+dequeue(a);
+print_queue(a);
+
+//3.22
+function make_queue_internal(){
+    function front_ptr(...) ...
+    function rear_ptr(...) ...
+    //definitions of internal functions
+    function dispatch(m) ...
+    return dispatch;
+}
+
+//dequeue
