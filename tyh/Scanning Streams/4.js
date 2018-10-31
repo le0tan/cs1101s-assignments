8fc3be3ea5d6399e04b868952d9a5193170eb15c
Tan Yuanhong <leotan.yh@gmail.com>
2018-10-31 13:07:03 +0800

    meta-circular tyh

--- tyh/Scanning Streams/4.js
+++ tyh/Scanning Streams/4.js
@@ -2,16 +2,18 @@
 
 function make_substrings(lst) {
     // Your solution here
-    // display(lst);
-    if(is_empty_list(lst)){
-        return stream();
-    } else {
-        const cur = make_substrings(tail(lst));
-        const prep = stream_append(stream(''), cur);
-        const h = head(lst);
-        const res = stream_map(x=>h+x, prep);
-        return stream_append(res, cur);
+    function h(xs){
+        if(is_empty_list(xs)){
+            return stream();
+        } else {
+            const cur = h(tail(xs));
+            const prep = stream_append(stream(''), cur);
+            const hd = head(xs);
+            const res = stream_map(x=>hd+x, prep);
+            return stream_append(res, cur);
+        }    
     }
+    return pair("", () => h(lst));
 }
 
 // make_substrings(list('a','b','c'));
